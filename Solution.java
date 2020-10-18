83e733587582b30cb17255dac564a99e74c45245
xzzxwmsl <1365303748@qq.com>
2020-10-18 21:03:12 +0800

    14.最长公共前缀

--- Solution.java
+++ Solution.java
@@ -1,16 +1,18 @@
 public class Solution {
 
     public static void main(String[] args) {
-        ListNode head = new ListNode(1);
-        ListNode p1 = new ListNode(2);
-        ListNode p2 = new ListNode(3);
-        ListNode p3 = new ListNode(4);
-        ListNode p4 = new ListNode(5);
-        head.add(p1);
-        p1.add(p2);
-        p2.add(p3);
-        p3.add(p4);
-        visitList(swapPairs(head));
+        // ListNode head = new ListNode(1);
+        // ListNode p1 = new ListNode(2);
+        // ListNode p2 = new ListNode(3);
+        // ListNode p3 = new ListNode(4);
+        // ListNode p4 = new ListNode(5);
+        // head.add(p1);
+        // p1.add(p2);
+        // p2.add(p3);
+        // p3.add(p4);
+        // visitList(swapPairs(head));
+        String[] s = new String[]{"","flow","flight"};
+        System.out.println(fun(s));
 
     }
 
@@ -27,6 +29,31 @@ public class Solution {
             p = p1;
         }
         return newHead.next;
+    }
+
+    public static String fun(String[] strs) {
+        StringBuilder re = new StringBuilder();
+
+        try {
+            int flag = 0;
+            boolean over = false;
+            while (true) {
+                char c = strs[0].charAt(flag);
+                for (String s : strs) {
+                    if (c != s.charAt(flag)) {
+                        over = true;
+                        break;
+                    }
+                }
+                if(over) break;
+                flag++;
+                re.append(c);
+            }
+        } catch (Exception e) {
+
+        }
+
+        return re.toString();
     }
 
     public static void visitList(ListNode listNode) {
